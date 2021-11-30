4d7a4c42dcb30a3263d5491c8f513d2d1bad9179
leewoooo <leecoding2285@gmail.com>
2021-11-30 21:13:26 +0900

    ✅Update: parse_address

--- doc/new/parse_address/parseaddress_test.go
+++ doc/new/parse_address/parseaddress_test.go
@@ -3,6 +3,7 @@ package parseaddress
 import (
 	"log"
 	"net/mail"
+	"regexp"
 	"testing"
 )
 
@@ -25,5 +26,28 @@ func TestParseAddress(t *testing.T) {
 		}
 
 		log.Printf("name: %s, address: %s", addressStruct.Name, addressStruct.Address)
+	}
+}
+
+func TestEmailReg(t *testing.T) {
+	emailreg := `^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`
+
+	given := []string{
+		"Alice <alice@example.com>",
+		"<alice@example.com>",
+		"alice@example.com",
+		"alice@example",
+		"bad-example",
+		"",
+		"@",
+	}
+
+	for _, v := range given {
+		ok, err := regexp.MatchString(emailreg, v)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("%s Match result: %v", v, ok)
 	}
 }
